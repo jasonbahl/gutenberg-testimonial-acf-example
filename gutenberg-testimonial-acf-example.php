c7d2a579041ffb2985c83b885d86b28f07dab4d2
Jason Bahl <jasonbahl@mac.com>
2018-11-14 11:33:41 -0700

    Step 2: Block Markup and Styles
    In this step, we write the markup we want to use to output our Block in our theme. This will also be the same markup used for previewing the component while editing in Gutenberg.
    
    We define a few variables that we will want to be editable when interacting with the block: $testimonial, $alignment, $avatar_url, $name, $text_color and $background_color. For now, we've hard-coded their values so we can test our markup and make sure it looks ok.
    
    For styling, we simply output a <style> tag along with our block markup, and we use some of our variables to make the styles dynamic.
    
    At this point, we can see our Block taking shape, both in the Gutenberg UI and in our Theme when we save a post and look at it.

--- gutenberg-testimonial-acf-example.php
+++ gutenberg-testimonial-acf-example.php
@@ -32,7 +32,67 @@ function my_acf_init() {
  * @param $block
  */
 function acf_testimonial_callback( $block ) {
+	$block_id = $block['id'];
+	$testimonial = 'This is the testimonial';
+	$alignment = 'right';
+	$avatar_url = 'https://placehold.it/100x100';
+	$name = 'Cited Person';
+	$text_color = 'white';
+	$background_color = 'blue';
 	?>
-	<h2><?php echo $block['name']; ?></h2>
+	<div id="<?php echo 'acf-testimonial-' . esc_attr( $block_id ); ?>" class="acf-testimonial">
+		<div class="acf-testimonial-text">
+			<?php echo esc_html( $testimonial ); ?>
+		</div>
+		<div class="acf-testimonial-info <?php echo esc_attr( $alignment ); ?>">
+			<div class="acf-testimonial-avatar-wrap">
+				<img src="<?php echo esc_url( $avatar_url ); ?>"/>
+			</div>
+			<h2 class="acf-testimonial-avatar-name">
+				<?php echo esc_html( $name ); ?>
+			</h2>
+		</div>
+	</div>
+	<style>
+		#<?php echo 'acf-testimonial-' . $block_id; ?>{
+			background-color: <?php echo $background_color; ?>;
+			color: <?php echo $text_color; ?>;
+			padding: 30px;
+		}
+		#<?php echo 'acf-testimonial-' . $block_id; ?> h2{
+			color: <?php echo $text_color; ?>;
+		}
+		.acf-testimonial-info{
+			position: relative;
+			display: inline-block;
+			width: 100%;
+			margin-top: 15px;
+			min-height: 55px;
+			padding-top: 5px;
+			line-height: 1.4;
+			text-align: left;
+		}
+		.acf-testimonial-info.right {
+			text-align: right;
+		}
+		.acf-testimonial-avatar-wrap {
+			position: absolute;
+			left: 0;
+			top: 0;
+		}
+		.acf-testimonial-info.right .acf-testimonial-avatar-wrap {
+			right: 0;
+		}
+		.acf-testimonial-avatar-wrap img {
+			max-width: 55px;
+		}
+		.acf-testimonial-avatar-name {
+			color: #ffffff;
+			margin-right: 81px;
+			margin-left: 0;
+			padding-left: 0;
+			left: 0;
+		}
+	</style>
 	<?php
 }
