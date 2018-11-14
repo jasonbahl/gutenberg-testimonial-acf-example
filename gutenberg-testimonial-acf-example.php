976a3b19af40165738c389d1f5852b4ebf9645ec
Jason Bahl <jasonbahl@mac.com>
2018-11-14 11:23:33 -0700

    Step 1: Register Block
    Here we hook into `acf/init` to ensure Advanced Custom Fields has been initialized
    Then we register the block using the ACF function `acf_register_block`
    This registers a block to Gutenberg, but also adds the block as a new Context for ACF Fields to be applied to.
    The `render_callback` is a standard Gutenberg block attribute that allows for dynamic rendering of blocks. Here, we define our callback to be `acf_testimonial_callback`, and inside that function we output some simple markup.
    
    At this point, we can now add the `ACF Testimonial Block` in Gutenberg, and we will see the name of the block rendered in the Editor and in our Theme when the post is saved.

--- gutenberg-testimonial-acf-example.php
+++ gutenberg-testimonial-acf-example.php
@@ -2,3 +2,37 @@
 /**
  * Plugin Name: Gutenberg Testimonial ACF Example
  */
+
+/**
+ * Register the block once ACF has initialized
+ */
+add_action( 'acf/init', 'my_acf_init' );
+function my_acf_init() {
+
+	// check function exists
+	if ( function_exists( 'acf_register_block' ) ) {
+
+		acf_register_block( [
+			'name'            => 'acf-testimonial',
+			'title'           => __( 'Testimonial - ACF' ),
+			'description'     => __( 'A custom testimonial block, using Advanced Custom Fields Pro.' ),
+			'render_callback' => 'acf_testimonial_callback',
+			'category'        => 'formatting',
+			'icon'            => 'admin-comments',
+			'keywords'        => [ 'testimonial', 'quote', 'acf' ],
+		] );
+	}
+
+}
+
+/**
+ * Render Callback for the block. This is what is output in the Theme AND
+ * in the preview within Gutenberg
+ *
+ * @param $block
+ */
+function acf_testimonial_callback( $block ) {
+	?>
+	<h2><?php echo $block['name']; ?></h2>
+	<?php
+}
